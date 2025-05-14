676b8cfc1aa99c1656692953f7bc1ab928a79539
aitbenmoumen <a.aitbenmoumen@gmail.com>
2025-05-14 14:26:51 +0100

    Add project files.

---
+++ deployment_CI_CD/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace deployment_CI_CD.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
